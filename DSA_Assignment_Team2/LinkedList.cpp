7d2cf405a113932a128803043cabde1f321a0e39
Xavier Lim <s10242016@connect.np.edu.sg>
2023-08-02 17:02:56 +0800

    added back code accidentally removed

--- DSA_Assignment_Team2/LinkedList.cpp
+++ DSA_Assignment_Team2/LinkedList.cpp
@@ -1,92 +1,112 @@
-#include "Order.h"
+#include "LinkedList.h"
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <iomanip>
-#include <sstream> 
+#include "Restaurant.h"
+#include "OrderItem.h"
 
 using namespace std;
 
-Order::Order() {}
-
-Order::Order(int id, int c, LinkedList<OrderItem>& o, int r, double p, const string& s)
-{
-	orderID = id;
-	customerID = c;
-	orderItemList = o;
-	restaurantID = r;
-	totalPrice = p;
-	status = s;
+template <class T>
+LinkedList<T>::LinkedList() {
+	head = nullptr;
+	size = 0;
 }
 
-int Order::getOrderID() const
-{
-	return orderID;
-}
-
-int Order::getCustomerID() const
-{
-	return customerID;
-}
-
-LinkedList<OrderItem> Order::getOrderItemList() const
-{
-	return orderItemList;
-}
-
-int Order::getRestaurantID() const
-{
-	return restaurantID;
-}
-
-double Order::getTotalPrice() const
-{
-	return totalPrice;
-}
-
-string Order::getStatus() const
-{
-	return status;
-}
-
-/* GOT ERROR AND NOT DONE YET
-Queue Order::GetIncomingOrders(const string& orderFileName) {
-	Queue queue;
-
-	ifstream file(orderFileName);
-	if (file.is_open()) {
-		string header;
-		getline(file, header);
-
-		string line;
-		while (getline(file, line)) {
-			istringstream iss(line);
-			string orderIDStr, customerIDStr, foodItemIDStr, restaurantIDStr, orderStatusStr, totalPriceStr;
-
-			getline(iss, orderIDStr, ',');
-			getline(iss, customerIDStr, ',');
-			getline(iss, foodItemIDStr, ',');
-			getline(iss, restaurantIDStr, ',');
-			getline(iss, orderStatusStr, ',');
-			getline(iss, totalPriceStr, ',');
-
-			if (orderStatusStr != "Prepared") {
-				orderID = stoi(orderIDStr);
-				customerID = stoi(customerIDStr);
-				// how to use the linkedlist?
-				restaurantID = stoi(restaurantIDStr);
-				status = orderStatusStr;
-				totalPrice = stod(totalPriceStr);
-			}
-
-		}
+template <class T>
+LinkedList<T>::~LinkedList() {
+	Node* temp = head;
+	while (head != nullptr) {
+		head = head->next;
+		delete temp;
+		temp = head;
 	}
-	return queue;
 }
-*/
 
+template <class T>
+bool LinkedList<T>::isEmpty() const {
+	return size == 0;
+}
 
+template <class T>
+int LinkedList<T>::getLength() const {
+	return size;
+}
 
+template <class T>
+bool LinkedList<T>::retrieve(int index, T& item) const {
+	if (index < 0 || index >= size) {
+		return false;
+	}
 
+	Node* current = head;
+	for (int i = 0; i < index; i++) {
+		current = current->next;
+	}
 
+	item = current->item;
+	return true;
+}
 
+template <class T>
+T LinkedList<T>::retrieve(int index) const {
+	if (index < 0 || index >= size) {
+		throw out_of_range("Index out of range");
+	}
+
+	Node* current = head;
+	for (int i = 0; i < index; i++) {
+		current = current->next;
+	}
+
+	return current->item;
+}
+
+template <class T>
+bool LinkedList<T>::insert(T item) {
+	Node* newNode = new Node;
+	newNode->item = item;
+	newNode->next = nullptr;
+
+	if (head == nullptr) {
+		head = newNode;
+	}
+	else {
+		Node* current = head;
+		while (current->next != nullptr) {
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+
+	size++;
+	return true;
+}
+
+template <class T>
+bool LinkedList<T>::remove(int index) {
+	if (index < 0 || index >= size) {
+		return false;
+	}
+
+	Node* current = head;
+	Node* previous = nullptr;
+	for (int i = 0; i < index; i++) {
+		previous = current;
+		current = current->next;
+	}
+
+	if (previous == nullptr) {
+		head = current->next;
+	}
+	else {
+		previous->next = current->next;
+	}
+
+	delete current;
+	size--;
+	return true;
+}
+
+// Explicit instantiation for supported types
+template class LinkedList<Restaurant>;
+template class LinkedList<OrderItem>;
--- DSA_Assignment_Team2/Order.cpp
+++ DSA_Assignment_Team2/Order.cpp
@@ -1,6 +1,9 @@
 #include "Order.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <iomanip>
+#include <sstream> 
 
 using namespace std;
 
@@ -46,6 +49,39 @@ string Order::getStatus() const
 	return status;
 }
 
+Queue Order::GetIncomingOrders(const string& orderFileName) {
+	Queue queue;
+
+	ifstream file(orderFileName);
+	if (file.is_open()) {
+		string header;
+		getline(file, header);
+
+		string line;
+		while (getline(file, line)) {
+			istringstream iss(line);
+			string orderIDStr, customerIDStr, foodItemIDStr, restaurantIDStr, orderStatusStr, totalPriceStr;
+
+			getline(iss, orderIDStr, ',');
+			getline(iss, customerIDStr, ',');
+			getline(iss, foodItemIDStr, ',');
+			getline(iss, restaurantIDStr, ',');
+			getline(iss, orderStatusStr, ',');
+			getline(iss, totalPriceStr, ',');
+
+			if (orderStatusStr != "Prepared") {
+				orderID = stoi(orderIDStr);
+				customerID = stoi(customerIDStr);
+				// how to use the linkedlist?
+				restaurantID = stoi(restaurantIDStr);
+				status = orderStatusStr;
+				totalPrice = stod(totalPriceStr);
+			}
+
+		}
+	}
+	return queue;
+}
 
 
 
